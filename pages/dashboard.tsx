3756f03f0f0870d89b96d7006f79a27d95d3a0c5
alfieqashwa <alfieqashwa@gmail.com>
2020-11-27 22:48:55 +0700

    restricted the dashboard page for non-users

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,28 +1,47 @@
-import React from 'react';
+import { FC, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { GetStaticProps } from 'next';
 import { PrismaClient } from '@prisma/client';
+import { useSession } from 'next-auth/client';
 
 import Layout from 'components/Layout';
 
-const Dashboard = ({ users }) => {
+const Dashboard: FC<{ allUsers: [UserProps] }> = ({ allUsers }) => {
+  const [session, loading] = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!(session || loading)) {
+      router.push('/');
+    }
+  }, [session, loading]);
+
+  const user = allUsers.filter((user) => user.email === session?.user.email);
   return (
-    <Layout title='Dashboard' profileImg={users[0].image}>
+    <Layout title='Dashboard' profileImg={user[0]?.image}>
       <h1 className='text-2xl'>Dashboard Page</h1>
       <br />
-      <code>{JSON.stringify(users, null, 4)}</code>
+      <code>{JSON.stringify(user, null, 4)}</code>
     </Layout>
   );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
   const prisma = new PrismaClient();
-  const users = await prisma.user.findMany({
+  const allUsers = await prisma.user.findMany({
     select: { id: true, email: true, image: true },
   });
 
   return {
-    props: { users },
+    props: { allUsers },
   };
+};
+
+export type UserProps = {
+  id?: number;
+  email: string;
+  image?: string | null;
+  entityImage?: string | null;
 };
 
 export default Dashboard;
