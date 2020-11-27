df9a29de13d6ae7efca8e74a40d91a28fa8c58f8
alfieqashwa <alfieqashwa@gmail.com>
2020-11-27 23:21:58 +0700

    playing w/ tailwind

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -19,9 +19,36 @@ const Dashboard: FC<{ allUsers: [UserProps] }> = ({ allUsers }) => {
   const user = allUsers.filter((user) => user.email === session?.user.email);
   return (
     <Layout title='Dashboard' profileImg={user[0]?.image}>
-      <h1 className='text-2xl'>Dashboard Page</h1>
-      <br />
-      <code>{JSON.stringify(user, null, 4)}</code>
+      <div className='text-center'>
+        <h1 className='text-2xl'>Dashboard Page</h1>
+        <div className='mt-8 space-y-4'>
+          <h1 className='text-2xl capitalize'>from prisma database</h1>
+          <ul className='px-2 py-4 space-y-8 text-xl'>
+            {allUsers.map((user) => (
+              <li
+                key={user.id}
+                className='flex items-center justify-center space-x-2'>
+                <img
+                  className='h-16 border-4 border-pink-400 rounded-full '
+                  src={user.image}
+                  alt={user.name}
+                />
+                <div>
+                  <h2>Email: {user.email}</h2>
+                  <h2>Name: {user.name}</h2>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className='mt-8 space-y-4'>
+          <h1 className='text-2xl capitalize'>from user session</h1>
+          <div className='text-xl'>
+            {JSON.stringify(session?.user.email, null, 4)}
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
@@ -29,7 +56,7 @@ const Dashboard: FC<{ allUsers: [UserProps] }> = ({ allUsers }) => {
 export const getStaticProps: GetStaticProps = async () => {
   const prisma = new PrismaClient();
   const allUsers = await prisma.user.findMany({
-    select: { id: true, email: true, image: true },
+    select: { id: true, email: true, name: true, image: true },
   });
 
   return {
@@ -40,6 +67,7 @@ export const getStaticProps: GetStaticProps = async () => {
 export type UserProps = {
   id?: number;
   email: string;
+  name: string;
   image?: string | null;
   entityImage?: string | null;
 };
